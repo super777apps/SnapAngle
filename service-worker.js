03ca7c77f9a1f2ef3ff90b54c479d1637fe3b1c1
super777apps <super777apps@gmail.com>
2026-02-12 00:38:30 +1100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "snapangle-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/app.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
